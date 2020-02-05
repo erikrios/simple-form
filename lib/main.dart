565072a6123807e04668169c735dc50e766e9b29
erikrios <erikriosetiawan49@gmail.com>
2020-02-05 21:49:48 +0700

    Call validate() method in the FormState object

--- lib/main.dart
+++ lib/main.dart
@@ -17,47 +17,64 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void validateInput() {
+    FormState form = this.formKey.currentState;
+    ScaffoldState scaffold = this.scaffoldKey.currentState;
+
+    SnackBar message = SnackBar(content: Text('Proses validasi sukses...'));
+
+    if (form.validate()) {
+      scaffold.showSnackBar(message);
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: Text('Demo Form'),
         ),
-        body: Form(
-            child: Column(
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Nama'),
-              keyboardType: TextInputType.text,
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Nama tidak boleh kosong';
-                }
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'No. HP'),
-              keyboardType: TextInputType.phone,
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'No. HP tidak boleh kosong';
-                }
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-              validator: (String value) {
-                if (value.isEmpty) {
-                  return 'Email tidak boleh kosong';
-                }
-              },
-            ),
-            Container(height: 10.0),
-            RaisedButton(
-              onPressed: null,
-              child: Text('Validasi Data'),
-            )
-          ],
-        )),
+        body: Container(
+          margin: EdgeInsets.all(15.0),
+          child: Form(
+              child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Nama'),
+                keyboardType: TextInputType.text,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Nama tidak boleh kosong';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'No. HP'),
+                keyboardType: TextInputType.phone,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'No. HP tidak boleh kosong';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (String value) {
+                  if (value.isEmpty) {
+                    return 'Email tidak boleh kosong';
+                  }
+                },
+              ),
+              Container(height: 10.0),
+              RaisedButton(
+                onPressed: validateInput,
+                child: Text('Validasi Data'),
+              )
+            ],
+          )),
+        ),
       );
 }
